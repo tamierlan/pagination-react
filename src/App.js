5e8a17d450d43e5787cc11c05d6276de376cecf6
Your Name <you@example.com>
2020-11-22 16:54:58 -0500

    startingletlix

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ function App() {
   const [ data, setData ] = useState([]);
   const [ page, setPage ] = useState(1);
   const [ api_key, setApi_key ] = useState('df8b08ecb436696fee41a00f8d87a540');
-  const [ loading, setLoading ] = useState(true)
+  const [ loading, setLoading ] = useState(false)
   const [ language, setLanguage ] = useState('en');
   const [ total_pages, setTotal_pages ] = useState()
+  const [start, setStart] = useState(true);
 
 
   const getData = () => {
@@ -27,7 +28,10 @@ function App() {
   }
 
   useEffect(() => {
-    getData()
+    setTimeout(() => {
+      getData()
+      setStart(false)
+    }, 3000)
   }, [])
 
 
@@ -45,8 +49,11 @@ function App() {
   }
 
   console.log(data)
-
   return (
+    start
+    ?
+    <img src='https://media.giphy.com/media/JnvHE3lTHPr3WrSsrl/giphy.gif' alt='start' style={{height: '100vh', width: '100%'}}  />
+    :
     <div className="App">
       <div className='navbar'>
         <img src='https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2012/10/22/1350920078216/Silhouettes-from-popular--003.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=b9349a5eb98ad0f6484affb6bfd03c9c' alt='logo' style={{height: '55px'}}  />
@@ -84,7 +91,7 @@ function App() {
                 <div className="column">
                   <div className="card">
                     <div className='name'>{actor.name}</div>
-                    <img src={`http://image.tmdb.org/t/p/w185/${actor.profile_path}`} alt='image' />
+                    <img src={actor.profile_path ? 'http://image.tmdb.org/t/p/w185/' + actor.profile_path : 'https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2012/10/22/1350920078216/Silhouettes-from-popular--003.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=b9349a5eb98ad0f6484affb6bfd03c9c'} alt='image' />
                   </div>
                 </div>
               </div>
